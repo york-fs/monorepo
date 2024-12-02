029d79c52350ad9c8da9b2713979ae9e93965516
Owen Smith <owen@owensmith.uk>
2024-12-02 00:42:36 +0000

    config: Add ERPM conversion factor

--- src/config.hh
+++ src/config.hh
@@ -6,4 +6,7 @@ namespace config {
 
 constexpr std::uint8_t k_dti_can_id = 0x5;
 
+// RPM to ERPM conversion factor. Emrax 228 has 10 motor pole pairs.
+constexpr std::uint8_t k_erpm_factor = 10;
+
 } // namespace config
