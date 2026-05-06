3bc8fb0bf0ca76bbfbc9af7d4f55ce7dcff577ac
Owen Smith <owen@owensmith.uk>
2026-05-06 15:53:21 +0100

    scripts/merge_bom: Use new BOM format

--- scripts/merge_bom.py
+++ scripts/merge_bom.py
@@ -5,14 +5,10 @@ import sys
 
 boms = [pd.read_csv(csv) for csv in sys.argv[1:]]
 concatenated = pd.concat(boms, ignore_index=True)
-
-merged = (
-    concatenated.groupby(["Value", "Footprint", "Mfr", "Mfr #"], dropna=False)
-    .agg({
-        "Qty": "sum",
-        "Dist": lambda x: ", ".join(sorted(set(x.dropna()))),
-        "Dist #": lambda x: ", ".join(sorted(set(x.dropna()))),
-    })
-)
-
-print(merged.to_csv())
+grouped = concatenated.groupby(['Value', 'Footprint', 'Mfr', 'Mfr #'], dropna=False).agg({
+    'Qty': 'sum',
+    'DigiKey #': 'first',
+    'Mouser #': 'first',
+    'LCSC #': 'first',
+})
+print(grouped.to_csv())
