66c707f3b4e5106bea349d9da91eb277a036a7cb
Owen Smith <owen@owensmith.uk>
2026-07-05 16:56:53 +0100

    dti: Add some more decode tests

--- test/dti_test.cc
+++ test/dti_test.cc
@@ -52,7 +52,7 @@ TEST(DtiBuild, SetDriveEnabled) {
               can::build_extended(0x0c20, std::to_array<std::uint8_t>({0x01})));
 }
 
-TEST(DtiParse, GeneralData1) {
+TEST(DtiDecode, GeneralData1) {
     const auto message_bytes = std::to_array<std::uint8_t>({0x00, 0x00, 0x24, 0x5e, 0x00, 0x71, 0x01, 0x86});
     const auto frame = can::build_extended(0x2022, message_bytes);
     const auto gd1 = can::decode<dti::GeneralData1>(0x22, frame);
@@ -62,7 +62,7 @@ TEST(DtiParse, GeneralData1) {
     EXPECT_EQ(gd1->input_voltage, 390);
 }
 
-TEST(DtiParse, GeneralData2) {
+TEST(DtiDecode, GeneralData2) {
     const auto message_bytes = std::to_array<std::uint8_t>({0x00, 0x5c, 0x00, 0x11});
     const auto frame = can::build_extended(0x2122, message_bytes);
     const auto gd2 = can::decode<dti::GeneralData2>(0x22, frame);
@@ -71,7 +71,7 @@ TEST(DtiParse, GeneralData2) {
     EXPECT_EQ(gd2->dc_current, 17);
 }
 
-TEST(DtiParse, GeneralData3) {
+TEST(DtiDecode, GeneralData3) {
     const auto message_bytes = std::to_array<std::uint8_t>({0x01, 0x53, 0x01, 0x17, 0x04});
     const auto frame = can::build_extended(0x2222, message_bytes);
     const auto gd3 = can::decode<dti::GeneralData3>(0x22, frame);
@@ -81,7 +81,7 @@ TEST(DtiParse, GeneralData3) {
     EXPECT_EQ(gd3->fault_code, dti::FaultCode::Overcurrent);
 }
 
-TEST(DtiParse, GeneralData5) {
+TEST(DtiDecode, GeneralData5) {
     const auto message_bytes = std::to_array<std::uint8_t>({0x38, 0xd8, 0xaa, 0x01, 0xaa, 0x05, 0xff, 0x18});
     const auto frame = can::build_extended(0x2422, message_bytes);
     const auto gd5 = can::decode<dti::GeneralData5>(0x22, frame);
@@ -102,6 +102,21 @@ TEST(DtiParse, GeneralData5) {
     EXPECT_FALSE(gd5->limit_flags.is_set(dti::Limit::RpmMaximum));
     EXPECT_TRUE(gd5->limit_flags.is_set(dti::Limit::PowerLimit));
     EXPECT_EQ(gd5->can_map_version, 24);
+}
+
+TEST(DtiDecode, GeneralData5_BadLength) {
+    const auto message_bytes = std::to_array<std::uint8_t>({0x38, 0xd8, 0xaa, 0x01, 0xaa, 0x05, 0x18});
+    const auto frame = can::build_extended(0x2422, message_bytes);
+    EXPECT_FALSE(can::decode<dti::GeneralData5>(0x22, frame));
+}
+
+TEST(DtiDecode, UnknownMessageType) {
+    const auto message_bytes = std::to_array<std::uint8_t>({0x12, 0x34});
+    const auto frame = can::build_extended(0x4022, message_bytes);
+    EXPECT_FALSE(can::decode<dti::GeneralData1>(0x22, frame));
+    EXPECT_FALSE(can::decode<dti::GeneralData2>(0x22, frame));
+    EXPECT_FALSE(can::decode<dti::GeneralData3>(0x22, frame));
+    EXPECT_FALSE(can::decode<dti::GeneralData5>(0x22, frame));
 }
 
 } // namespace
