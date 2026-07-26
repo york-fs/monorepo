1a58ff5b9050d8a3dafc872b48529539957f95e4
Owen Smith <owen@owensmith.uk>
2026-07-26 19:15:26 +0100

    config: Adjust CAN node IDs

--- src/config.hh
+++ src/config.hh
@@ -8,17 +8,12 @@ namespace config {
 
 constexpr auto k_can_speed = can::Speed::_500;
 
-constexpr std::uint8_t k_front_can_id = 0x1;
-
-constexpr std::uint8_t k_rear_can_id = 0x2;
-
+constexpr std::uint8_t k_rear_can_id = 0x1;
+constexpr std::uint8_t k_front_can_id = 0x2;
+constexpr std::uint8_t k_bms_can_id = 0x3;
+constexpr std::uint8_t k_precharge_can_id = 0x4;
 constexpr std::uint8_t k_dti_can_id = 0x5;
-
-constexpr std::uint8_t k_bms_can_id = 0x7;
-
-constexpr std::uint8_t k_precharge_can_id = 0x8;
-
-constexpr std::uint8_t k_charger_can_id = 0x10;
+constexpr std::uint8_t k_charger_can_id = 0x6;
 
 // RPM to ERPM conversion factor. Emrax 228 has 10 motor pole pairs.
 constexpr std::uint8_t k_erpm_factor = 10;
