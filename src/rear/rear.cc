10fbe69a82e8b422d56322756bdcab08baf5e18f
Owen Smith <owen@owensmith.uk>
2026-08-08 11:11:43 +0100

    rear: Add basic telemetry frame transmission

--- src/rear/rear.cc
+++ src/rear/rear.cc
@@ -5,12 +5,23 @@
 #include <rear/can_messages.hh>
 #include <stm32f103xb.h>
 
+#include <bit>
 #include <cstdint>
-#include <string_view>
+#include <span>
 
 using namespace rear;
 
 namespace {
+
+/**
+ * @brief The configured baud rate of the radio's UART.
+ */
+constexpr std::uint32_t k_radio_baud = 115200;
+
+/**
+ * @brief Telemetry radio send period in milliseconds.
+ */
+constexpr std::uint32_t k_radio_period = 100;
 
 /**
  * @brief Hard-coded value of the 3V3 rail powering the STM's ADC in 1 mV resolution.
@@ -23,7 +34,7 @@ hal::Gpio s_radio_cts(hal::GpioPort::A, 11);
 hal::Gpio s_radio_rts(hal::GpioPort::A, 12);
 
 freertos::Task<128> s_adc_task;
-freertos::Task<128> s_radio_task;
+freertos::Task<256> s_radio_task;
 freertos::Task<128> s_swd_task;
 
 void adc_task(void *) {
@@ -71,31 +82,82 @@ void radio_task(void *) {
     // Configure GPIOs.
     s_radio_tx.configure(hal::GpioOutputMode::AlternatePushPull, hal::GpioOutputSpeed::Max10);
     s_radio_rx.configure(hal::GpioInputMode::Floating);
-    s_radio_cts.configure(hal::GpioInputMode::PullUp);
-    s_radio_rts.configure(hal::GpioInputMode::PullUp);
+    s_radio_cts.configure(hal::GpioInputMode::Floating);
+    s_radio_rts.configure(hal::GpioInputMode::PullDown);
 
-    // Enable peripheral clock.
+    // Enable peripheral clocks.
+    RCC->AHBENR |= RCC_AHBENR_DMA1EN;
     RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
 
-    // Program 57600 baud rate.
-    USART1->BRR = 56000000 / 57600;
+    // Configure DMA with the transfer complete interrupt enabled.
+    std::array<std::uint8_t, 256> tx_bytes;
+    DMA1_Channel4->CPAR = std::bit_cast<std::uint32_t>(&USART1->DR);
+    DMA1_Channel4->CMAR = std::bit_cast<std::uint32_t>(tx_bytes.data());
+    DMA1_Channel4->CCR = DMA_CCR_MINC | DMA_CCR_DIR | DMA_CCR_TCIE;
 
-    // Enable transmit and receive with interrupts.
-    USART1->CR1 = USART_CR1_UE | USART_CR1_TXEIE | USART_CR1_RXNEIE | USART_CR1_TE | USART_CR1_RE;
+    // Program the configured baud rate.
+    USART1->BRR = 56000000 / k_radio_baud;
+
+    // Enable UART with DMA and transmission enabled.
+    USART1->CR3 = USART_CR3_DMAT;
+    USART1->CR1 = USART_CR1_UE | USART_CR1_TE;
 
     // Enable the interrupt.
-    hal::irq_enable(USART1_IRQn, 7);
+    hal::irq_enable(DMA1_Channel4_IRQn, 7);
 
+    // Keep track of the number of transmission windows we've missed.
+    std::uint8_t missed_tx_count = 0;
+
+    freertos::PeriodScheduler scheduler;
     while (true) {
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        scheduler.delay_until_ms(k_radio_period);
+
+        // Don't transmit if the radio's UART buffer is near full.
+        if (s_radio_cts.read()) {
+            missed_tx_count++;
+            continue;
+        }
+
+        // Build a telemetry frame with the data offset by one byte to allow for the first COBS code. The size is also
+        // limited to 3 bytes fewer to allow for the overall COBS overhead.
+        util::Stream stream(std::span<std::uint8_t>(tx_bytes).subspan(1, 253));
+        stream.write_be<std::uint16_t>(freertos::uptime_ms() / 1000);
+        stream.write_byte(missed_tx_count);
+
+        // Append a checksum.
+        stream.write_be(freertos::in_critical_section([&] {
+            return hal::crc_compute(stream.bytes());
+        }));
+
+        // COBS encode the data array in place.
+        std::uint32_t write_index = 1;
+        std::uint32_t code_index = 0;
+        tx_bytes[code_index] = 1;
+        for (std::uint32_t i = 1; i <= stream.head(); i++) {
+            const std::uint8_t byte = tx_bytes[i];
+            if (byte != 0) {
+                tx_bytes[write_index++] = byte;
+                tx_bytes[code_index]++;
+            } else {
+                code_index = write_index++;
+                tx_bytes[code_index] = 1;
+            }
+        }
+
+        // Append the frame delimiter.
+        tx_bytes[write_index++] = 0;
+
+        // Start the DMA transfer.
+        USART1->SR = 0;
+        DMA1_Channel4->CNDTR = write_index;
+        DMA1_Channel4->CCR |= DMA_CCR_EN;
     }
 }
 
 void swd_task(void *) {
-    TickType_t last_schedule_time = xTaskGetTickCount();
+    freertos::PeriodScheduler scheduler;
     while (true) {
-        xTaskDelayUntil(&last_schedule_time, pdMS_TO_TICKS(1000));
-
+        scheduler.delay_until_ms(1000);
         hal::swd_printf("--------------------------------\n");
         hal::swd_printf("Uptime: %u\n", freertos::uptime_ms() / 1000);
 
@@ -107,14 +169,10 @@ void swd_task(void *) {
 
 } // namespace
 
-extern "C" void USART1_IRQHandler() {
-    const auto sr = USART1->SR;
-    if ((sr & USART_SR_TXE) != 0) {
-        USART1->DR = 'a';
-    }
-    if ((sr & USART_SR_RXNE) != 0) {
-        hal::swd_printf("received byte: 0x%x\n", USART1->DR);
-    }
+extern "C" void DMA1_Channel4_IRQHandler() {
+    // Clear the pending flag and disable the channel.
+    DMA1->IFCR |= DMA_IFCR_CTCIF4;
+    DMA1_Channel4->CCR &= ~DMA_CCR_EN;
 }
 
 void vApplicationIdleHook() {
